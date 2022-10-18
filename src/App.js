4f96ff97fc4d0a3d9f97a70fd84e6549598f1772
aki024 <aleksa.simicc7@gmail.com>
2022-10-18 12:48:06 +0200

    proba

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import DvgBrojke from "./components/Brojke/DvgBrojke";
 import Header from "./components/Header/Header";
+import FrontPageIntro from "./components/FrontPageIntro";
 import "./styles/App.scss";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <FrontPageIntro />
       {/* <DvgBrojke /> */}
     </div>
   );
