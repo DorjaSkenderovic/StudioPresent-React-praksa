651a337ed32937088c9451fabaa500e897e59805
aki024 <aleksa.simicc7@gmail.com>
2022-10-18 13:00:52 +0200

    fix: margin

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-<<<<<<< HEAD
-// import DvgBrojke from "./components/Brojke/DvgBrojke";
-=======
+import DvgBrojke from "./components/Brojke/DvgBrojke";
 import FrontpageIntro from "./components/Frontpage Intro/FrontpageIntro";
->>>>>>> c47c7066e632bfcff6dd2ddfe413638cf7825f78
 import Header from "./components/Header/Header";
-import FrontPageIntro from "./components/FrontPageIntro";
 import "./styles/App.scss";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-<<<<<<< HEAD
-      <FrontPageIntro />
-      {/* <DvgBrojke /> */}
-=======
       <FrontpageIntro />
->>>>>>> c47c7066e632bfcff6dd2ddfe413638cf7825f78
+      <DvgBrojke />
     </div>
   );
 };
