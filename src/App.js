2d3d796dece388106485d79f4d0239d8109bf26f
DorjaSkenderovic <dorjaskenderovic@gmail.com>
2022-10-18 14:59:48 +0200

    added Products component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import FrontpageIntro from "./components/Frontpage Intro/FrontpageIntro";
 import Header from "./components/Header/Header";
-import Products from "./components/Products/Products";
 import "./styles/App.scss";
 
 const App = () => {
@@ -8,7 +7,6 @@ const App = () => {
     <div className="App">
       <Header />
       <FrontpageIntro />
-      <Products />
     </div>
   );
 };
