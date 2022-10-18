82768fc071321fe090551d35cbdb7fd915be8a46
DorjaSkenderovic <dorjaskenderovic@gmail.com>
2022-10-18 21:51:09 +0200

    added Manufacturers component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import DvgBrojke from "./components/Brojke/DvgBrojke";
 import Footer from "./components/Footer/Footer";
 import FrontpageIntro from "./components/Frontpage Intro/FrontpageIntro";
 import Header from "./components/Header/Header";
+import Manufacturers from "./components/Manufacturers/Manufacturers";
 import Products from "./components/Products/Products";
 import "./styles/App.scss";
 
@@ -11,9 +12,9 @@ const App = () => {
       <Header />
       <FrontpageIntro />
       <DvgBrojke />
-
       <Products />
-
+      <Manufacturers />
+      <Footer />
     </div>
   );
 };
