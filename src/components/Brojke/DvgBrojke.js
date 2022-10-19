f66a50a4f3eba63138530678c48a8419db177b46
DorjaSkenderovic <dorjaskenderovic@gmail.com>
2022-10-19 14:54:56 +0200

    counter

--- src/components/Brojke/DvgBrojke.js
+++ src/components/Brojke/DvgBrojke.js
@@ -1,6 +1,33 @@
 import styles from "./DvgBrojke.module.scss";
+import React from "react";
 
 const DvgBrojke = () => {
+
+  React.useEffect(() => {
+    const counters = document.querySelectorAll('.value');
+    const speed = 200;
+
+    counters.forEach(counter => {
+      const animate = () => {
+        const value = +counter.getAttribute('number');
+        const data = +counter.innerText;
+
+        const time = value / speed;
+        if (data < value) {
+          counter.innerText = Math.ceil(data + time);
+          setTimeout(animate, 1);
+        } else {
+          counter.innerText = value;
+        }
+
+      }
+
+      animate();
+    });
+  }, [])
+
+
+
   return (
     <div className={styles.dvgBrojke}>
       <div className={styles.dvgBrojkeTitle}>
@@ -12,27 +39,32 @@ const DvgBrojke = () => {
       <div className={styles.dvgBrojkeNumbers}>
         <div className={styles.dvgBrojkeNumberContainer}>
           <div className={styles.dvgBorder}>
-            <h3>
-              3000<span>m²</span>
-            </h3>
+            <div className={styles.firstBox}>
+              <h3 className="value" number="3000">
+                0
+              </h3><p>m²</p>
+            </div>
             <p>Magacinskog prostora</p>
           </div>
         </div>
         <div className={styles.dvgBrojkeNumberContainer}>
           <div className={styles.dvgBorder}>
-            <h3>20</h3>
+            <h3 className="value" number="20">
+              0</h3>
             <p>Vozila</p>
           </div>
         </div>
         <div className={styles.dvgBrojkeNumberContainer}>
           <div className={styles.dvgBorder}>
-            <h3>8</h3>
+            <h3 className="value" number="8">
+              0</h3>
             <p>Komercijalista</p>
           </div>
         </div>
         <div className={styles.dvgBrojkeNumberContainer}>
           <div className={styles.dvgBorder}>
-            <h3>170</h3>
+            <h3 className="value" number="170">
+              0</h3>
             <p>Brendova</p>
           </div>
         </div>
